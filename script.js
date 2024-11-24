5b49beb4ae3415c3819e4df2b9f6eb836edd4b6b
Alejandro Meneses Sánchez <alejandro214003@gmail.com>
2024-11-24 18:30:23 +0100

    Casi casi

--- script.js
+++ script.js
@@ -23,11 +23,37 @@ add.addEventListener("click", () => {
     const task = tasks.value;
     const notfinished = document.getElementById("not finished");  
     const tasknotfinished=document.createElement("div");
+    const edittask=document.createElement("div");
+    const deletetask=document.createElement("div");
+
+    edittask.innerText="✏️";
+    deletetask.innerText="🗑️";
+
+tasknotfinished.style.backgroundColor = `hsl(${Math.random() * 360}, 60%, 50%)`;
+
+    edittask.classList.add("edittask");
+    deletetask.classList.add("deletetask");
+
     tasknotfinished.classList.add("taskmove");
+    
     const taskonworkspan=document.createElement("span");
     taskonworkspan.innerText=task;
     tasknotfinished.appendChild(taskonworkspan);
+    tasknotfinished.appendChild(edittask);
+    tasknotfinished.appendChild(deletetask);
     notfinished.appendChild(tasknotfinished);
     tasks.value="";
+
+    deletetask.addEventListener("click", () => {    
+        notfinished.removeChild(tasknotfinished);
+    });
+
+    edittask.addEventListener("click", () => {
+        taskonworkspan.contentEditable = true;
+        taskonworkspan.focus();
+    });
+    
   
 });
+
+
